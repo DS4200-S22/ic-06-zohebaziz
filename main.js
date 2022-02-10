faa1aa7de0c993c6f72d764668c4a16c38dfb16e
zohebaziz <zaziz101@gmail.com>
2022-02-10 12:55:11 -0500

    done

--- main.js
+++ main.js
@@ -9,6 +9,11 @@ Modified: 12/06/21
 //Starter code for Button 1 click
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
+
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    document.getElementById("random_num").innerHTML = Math.random();
+}
+
+function button2Clicked() {
+    document.getElementById("circle_color").setAttribute("fill", "green")
 }
